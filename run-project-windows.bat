4757bc21afc2f6a7b497c61bd74f30264746ce8c
isaaccandido <isaac.guimaraescandido@gmail.com>
2024-10-28 17:51:15 -0300

    updates script to make sure docker is installed (windows)

--- run-project-windows.bat
+++ run-project-windows.bat
@@ -1,41 +1,35 @@
 @echo off
 setlocal
 
-set JAVA_VERSION=21
-set JAVA_URL=https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.zip
+set DOCKER_CMD=docker
+set TEMP_DOCKER_SCRIPT=get-docker.bat
+set DOCKER_URL=https://get.docker.com
 set JAVA_DIR=%~dp0java
-set TEMP_JAVA_DIR=%JAVA_DIR%\jdk-temp
 
-for /d %%D in ("%JAVA_DIR%\jdk-*") do (
-    set "EXISTING_JAVA=%%D"
-)
-
-if not defined EXISTING_JAVA (
-    echo Standalone Java not found. Downloading JDK %JAVA_VERSION%...
-
-    mkdir "%TEMP_JAVA_DIR%"
-
-    curl -L -o "%TEMP_JAVA_DIR%\jdk.zip" %JAVA_URL%
-
-    echo Extracting JDK...
-    powershell -command "Expand-Archive -Path '%TEMP_JAVA_DIR%\jdk.zip' -DestinationPath '%TEMP_JAVA_DIR%'"
-
-    for /d %%D in ("%TEMP_JAVA_DIR%\jdk-*") do (
-        move "%%D" "%JAVA_DIR%\jdk-%JAVA_VERSION%"
-        set "EXISTING_JAVA=%JAVA_DIR%\jdk-%JAVA_VERSION%"
-    )
-
-    del "%TEMP_JAVA_DIR%\jdk.zip"
-    rmdir /s /q "%TEMP_JAVA_DIR%"
-)
-
-set JAVA_HOME=%EXISTING_JAVA%
-set PATH=%JAVA_HOME%\bin;%PATH%
-
-java -version
+where %DOCKER_CMD% >nul 2>&1
 if %ERRORLEVEL% neq 0 (
-    echo Failed to set up Java. Exiting...
-    exit /b 1
+    echo Docker is not installed. Installing Docker...
+
+    curl -fsSL %DOCKER_URL% -o %TEMP_DOCKER_SCRIPT%
+    call %TEMP_DOCKER_SCRIPT%
+    del %TEMP_DOCKER_SCRIPT%
+
+    echo Docker installed successfully.
+) else (
+    echo Docker is already installed.
+)
+
+docker info >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Starting Docker service...
+    net start com.docker.service >nul 2>&1
+)
+
+net localgroup docker %USERNAME% >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo Adding current user to the 'docker' group...
+    net localgroup docker %USERNAME% /add >nul 2>&1
+    echo You may need to log out and log back in for group changes to take effect.
 )
 
 echo Running Spring Boot application...
